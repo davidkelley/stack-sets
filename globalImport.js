350685e47bd62d9eab5217e8fadda9d1b97888a9
davidkelley <david.james.kelley@gmail.com>
2018-04-13 16:51:40 +0100

    updates to global imports and layout

--- globalImport.js
+++ globalImport.js
@@ -1,7 +1,8 @@
 const response = require("cfn-response");
 const AWS = require("aws-sdk");
 exports.handler = (event, context) => {
-  const { SourceRegion } = event.ResourceProperties;
+  const { SourceRegion, Exports = ['.+'] } = event.ResourceProperties;
+  const filters = Exports.map(e => new RegExp(e, 'i'));
   const id = `custom:cloudformation:${SourceRegion}:exports`;
   const cloudformation = new AWS.CloudFormation({ region: SourceRegion });
   try {
@@ -10,7 +11,10 @@ exports.handler = (event, context) => {
         throw err;
       } else {
         const obj = {};
-        data.Exports.forEach(({ Name, Value }) => (obj[Name] = Value));
+        data.Exports.forEach(({ Name, Value }) => {
+          const matches = filters.filter(f => f.test(Name));
+          if (matches.length > 0) obj[Name] = Value;
+        });
         response.send(event, context, response.SUCCESS, obj, id);
       }
     });
